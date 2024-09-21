60a1fbce78f71845dd9758e42cd7819d0a8af976
Ksawlii <ksawlii.do.plikow@gmail.com>
2024-09-21 20:33:21 +0000

    fortuna3g: included (optional) sign keys

--- lineage_gprimeltexx.mk
+++ lineage_gprimeltexx.mk
@@ -26,4 +26,7 @@ PRODUCT_MODEL := SM-G530FZ
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_CHARACTERISTICS := phone
 
+# Sign (optional)
+-include vendor/extra/product.mk
+
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
